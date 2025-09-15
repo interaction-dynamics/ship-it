2f67f10d294d1b2f5aca7abd3900cb5ae66acfb0
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-09-15 08:29:19 -0400

    refactor: fix zod

--- src/entities/social.tsx
+++ src/entities/social.tsx
@@ -1,22 +1,22 @@
 import type { ReactNode } from 'react'
 import { z } from 'zod'
 
-export const SocialIconRenderer = z
-  .function()
-  .args(
+export const SocialIconRenderer = z.function({
+  input: [
     z.object({
       className: z.string(),
       'aria-hidden': z.literal('true'),
     }),
-  )
-  .returns(z.custom<ReactNode>())
+  ],
+  output: z.custom<ReactNode>(),
+})
 
 export type SocialIconRenderer = z.infer<typeof SocialIconRenderer>
 
 export const Social = z.object({
   name: z.string(),
-  url: z.string().url(),
-  icon: SocialIconRenderer.optional(),
+  url: z.url(),
+  icon: SocialIconRenderer,
 })
 
 export type Social = z.infer<typeof Social>
