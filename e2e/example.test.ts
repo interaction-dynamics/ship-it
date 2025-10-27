30985429ac25a53eac946fda81d2fa88045ee08c
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-10-27 07:12:37 -0400

    test: disable e2e tests

--- e2e/example.test.ts
+++ e2e/example.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test'
 
-test('should show the templates section', async ({ page }) => {
+test('should show the title', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
   // Find an element with the text 'About Page' and click on it
-  await expect(page.getByText('Templates')).toBeVisible()
+  await expect(page.getByText('Ship It')).toBeVisible()
 })
