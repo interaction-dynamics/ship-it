602e5fef80b39407550a8d33b81ca087b38d1794
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2026-03-14 09:34:11 -0400

    refactor: add vercel analytics

--- apps/web/src/adapters/analytics/analytics-script.tsx
+++ apps/web/src/adapters/analytics/analytics-script.tsx
@@ -1,11 +1,13 @@
 import { GoogleProvider } from './google';
 import { HotJar } from './hotjar';
+import { Analytics } from './vercel';
 
 export function AnalyticsScript() {
   return (
     <>
       <HotJar />
       <GoogleProvider />
+      <Analytics />
     </>
   );
 }
